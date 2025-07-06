f10d0ab01448746ffbe822a0fbab30baf3d653a5
Julio Gonzalez <gonzalez_julio01@student.smc.edu>
2025-07-05 21:11:20 -0700

    Add isEven(), square(), toCelsius(), wordLength() functions

--- journal.js
+++ journal.js
@@ -2,3 +2,23 @@
 function greet(name) {
   return "Hello, " + name + "!";
 }
+// Check if a number is even
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+// Calculate the square of a number
+function square(num) {
+  return num * num;
+}
+
+// Convert Fahrenheit to Celsius
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+// Get the length of a word
+function wordLength(word) {
+  return word.length;
+}
+
